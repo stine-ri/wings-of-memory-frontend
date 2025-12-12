ed2b998f2d7d97f08a41df70c8f0df66b2419afe
Christine Nyambwari <christinenyambwari@gmail.com>
2025-12-12 12:09:23 +0300

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,7 @@
-// main.tsx - UPDATED
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
 
-// REMOVE this empty import: import './index.css'
-// Since all CSS is in App.css, just import App.css:
-import './App.css'
+import App from './App.tsx'  // App.tsx will NOT import CSS
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
